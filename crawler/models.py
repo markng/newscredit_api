8a0159aebf03b333580d70e0cc4ce91641fb6d61
Mark Ng <mng@tolerance.markng.co.uk>
2009-01-09 14:38:24 +0000

    add admin site

--- crawler/models.py
+++ crawler/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 class CrawlSite(models.Model):
   """site from which we are crawling"""
   url = models.URLField("Site URL")
   feed_url = models.URLField("site rss/atom feed")
+admin.site.register(CrawlSite)
