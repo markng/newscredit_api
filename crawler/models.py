0424646ce41a87c5ab3758bb0f43dd4b503ef7e7
Mark Ng <mng@tolerance.markng.co.uk>
2009-01-09 13:29:52 +0000

    add model for newscredit sites

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class CrawlSite(models.Model):
+  """site from which we are crawling"""
+  url = models.URLField("Site URL")
+  feed_url = models.URLField("site rss/atom feed")
