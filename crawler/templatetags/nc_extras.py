ae87ac74913c82e8e0da3696b6e9204a302f15fe
Mark Ng <mark@markng.co.uk>
2009-03-24 15:25:06 +0000

    json tags

---
+++ crawler/templatetags/nc_extras.py
@@ -0,0 +1,12 @@
+from django import template
+from django.utils.safestring import mark_safe
+from django.utils.encoding import force_unicode
+import simplejson
+
+register = template.Library()
+
+@register.filter(name='jsonify')
+def jsonify(object):
+  """jsonify that object"""
+  return mark_safe(simplejson.dumps(object))
+jsonify.is_safe = True
