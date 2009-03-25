e08388bf67a90a46977a65402403e07e1aafee0d
Mark Ng <mark@markng.co.uk>
2009-03-25 12:01:00 +0000

    move stuff into localsettings

--- settings.py
+++ settings.py
@@ -3,55 +3,9 @@ import os
 from os import path as os_path
 PROJECT_PATH = os_path.abspath(os_path.split(__file__)[0])
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
-MANAGERS = ADMINS
-
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'crawler.sqlite'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'Europe/London'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '8^x5^gmzetwf%jqs)j)f1rb93g#7ccgg4h9d)!q!7qr6a59166'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -68,12 +22,6 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'newscredit_store.urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os_path.join(PROJECT_PATH, 'templates'),
-)
 
 INSTALLED_APPS = (
   'django.contrib.auth',
@@ -83,9 +31,27 @@ INSTALLED_APPS = (
   'django.contrib.admin',
   'django.contrib.databrowse',
   'newscredit_store.crawler',
+  'solango',
   'tagging',
   'extensions',
   'django_evolution',
 )
 
-MAXIMUM_CRAWLER_THREADS = 4
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os_path.join(PROJECT_PATH, 'templates'),
+)
+
+SEARCH_FACET_PARAMS = [
+    ("facet", "true"),             # basic faceting
+    ("facet.field", "model"),      # on type
+]
+
+SEARCH_SORT_PARAMS = {
+        "score desc": "Relevance",
+        "date desc" : "Date" # Added date
+}
+
+from localsettings import *
