30dbe3869995281c1f33ee9700ebb8af514214c0
Mark Ng <mark@markng.co.uk>
2009-04-22 08:54:29 +0100

    styling

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin, databrowse
+from os import path as os_path
+from django.conf import settings
 
 admin.autodiscover()
 
@@ -7,6 +9,8 @@ urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
     (r'^databrowse/(.*)', databrowse.site.root),
     (r'^$', 'django.views.generic.simple.direct_to_template', {'template' : 'index.html'}, 'home'),
+    (r'^images/(.*)$', 'django.views.static.serve', {'document_root': os_path.join(settings.PROJECT_PATH, 'media/images')}, 'images'),
+    (r'^stylesheets/(.*)$', 'django.views.static.serve', {'document_root': os_path.join(settings.PROJECT_PATH, 'media/stylesheets')}),
     (r'^search/', include('solango.urls')),
     url(r'^search.json/$', 'solango.views.select', {'template_name' : 'json.html'}, 'solango_search_json'),
     (r'^', include('crawler.urls')),
