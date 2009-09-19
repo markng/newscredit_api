fdf9b06195c44a14e29eb892ddbc53e00f64a2b5
Matt <matt@themattharris.com>
2009-09-19 08:55:33 +0800

    Whitespace and attributes fixes to relprinciples
    Signed-off-by: Mark Ng <mark@markng.co.uk>

--- locallibs/aump/relprinciples.py
+++ locallibs/aump/relprinciples.py
@@ -1,9 +1,9 @@
 #
-#	reltag.py
+#   relprinciples.py
 #
-#	David Janes
-#	BlogMatrix
-#	2005.12.06
+#   Matt Harris
+#   Tokofu
+#   2009.09.18
 #
 
 import sys
@@ -25,67 +25,65 @@ import hcard
 from microformat import Log
 
 class MicroformatRelPrinciples(microformat.Microformat):
-	def __init__(self, **args):
-		microformat.Microformat.__init__(self, root_name = "principles", uf_name = "rel-principles", root_type = "rel", **args)
+    def __init__(self, **args):
+        microformat.Microformat.__init__(self, root_name = "principles", uf_name = "rel-principles", root_type = "rel", **args)
 
         # self.CollectClassText('rating', text_type = microformat.TT_ABBR)
         # self.CollectClassText('best', text_type = microformat.TT_ABBR)
         # self.CollectClassText('worst', text_type = microformat.TT_ABBR)
 
-		self.CollectRelAttribute('principles', 'href')
-		self.CollectRelText('principles', as_name = 'principle-text', text_type = microformat.TT_STRING)
-		self.DeclareURI('authority')
+        self.CollectRelAttribute('principles', 'href')
+        self.CollectRelText('principles', as_name = 'principles', text_type = microformat.TT_STRING)
+        self.DeclareURI('authority')
 
-		self.DeclareTitle("name")
+        self.DeclareTitle("principles")
 
-	def Spawn(self):
-		mf = MicroformatRelPrinciples(page_uri = self.page_uri)
-		mf.data = self.data
-		mf.intermediates = self.intermediates
-		mf.root_element = self.root_element
-		self.Reset()
+    def Spawn(self):
+        mf = MicroformatRelPrinciples(page_uri = self.page_uri)
+        mf.data = self.data
+        mf.intermediates = self.intermediates
+        mf.root_element = self.root_element
+        self.Reset()
 
-		return	mf
+        return  mf
 
-	def AddResult(self, name, element, text):
-		if name == "principles":
-			tag_name = os.path.basename(text.rstrip('/'))
+    def AddResult(self, name, element, text):
+        if name == "principles":
+            uri = text
+            if self.page_uri:
+                uri = urlparse.urljoin(self.page_uri, uri)
+                
+            text = self.GetText(element)
+            microformat.Microformat.AddResult(self, "uri", element, uri)
 
-			uri = text
-			if self.page_uri:
-				uri = urlparse.urljoin(self.page_uri, uri)
-
-			microformat.Microformat.AddResult(self, "name", element, urllib.unquote(tag_name))
-			microformat.Microformat.AddResult(self, "uri", element, uri)
-		else:
-			microformat.Microformat.AddResult(self, name, element, text)
+        microformat.Microformat.AddResult(self, name, element, text)
 
 if __name__ == '__main__':
-	import microformat
-	microformat.debug = 1
+    import microformat
+    microformat.debug = 1
 
-	from optparse import OptionParser
+    from optparse import OptionParser
 
-	parser = OptionParser()
-	parser.add_option(
-		"", "--uri",
-		default = "",
-		dest = "page_uri",
-		help = "The URI of the page being processed")
-	parser.add_option(
-		"", "--verbose",
-		default = False,
-		action = "store_true",
-		dest = "verbose",
-		help = "print a lot about what is happening",
-	)
+    parser = OptionParser()
+    parser.add_option(
+        "", "--uri",
+        default = "",
+        dest = "page_uri",
+        help = "The URI of the page being processed")
+    parser.add_option(
+        "", "--verbose",
+        default = False,
+        action = "store_true",
+        dest = "verbose",
+        help = "print a lot about what is happening",
+    )
 
-	(options, args) = parser.parse_args()
-	Log.verbose = options.verbose
+    (options, args) = parser.parse_args()
+    Log.verbose = options.verbose
 
-	parser = MicroformatRelPrincples(page_uri = options.page_uri)
-	parser.PragmaCLI(args)
+    parser = MicroformatRelPrincples(page_uri = options.page_uri)
+    parser.PragmaCLI(args)
 
-	for resultd in parser.Iterate():
-		pprint.pprint(resultd, width = 1)
+    for resultd in parser.Iterate():
+        pprint.pprint(resultd, width = 1)
 
